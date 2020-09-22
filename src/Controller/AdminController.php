3724e9cab33d111427a2cc3305135e8f9de831a5
Adam <adam@clicct.com>
2020-09-22 17:04:27 -0400

    add admin panel

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Tour;
 
-class DefaultController extends AbstractController
+class AdminController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/Admin", name="ot_admin")
      */
     public function index()
     {
-        return $this->render('index.html.twig');
+        $tours = $this->getDoctrine()->getRepository(Tour::class)->findAll();
+
+        return $this->render('admin.html.twig', [
+            'tours' => $tours
+        ]);
     }
 }
