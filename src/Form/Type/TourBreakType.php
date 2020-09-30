6cbcf364cecc25e6976cbc9f4fb4275acc287c73
Adam <adam@clicct.com>
2020-09-30 16:31:48 -0400

    pricing tab works

--- src/Form/Type/TourBreakType.php
+++ src/Form/Type/TourBreakType.php
@@ -6,37 +6,35 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class TourSellsType extends AbstractType
+class TourBreakType extends AbstractType
 {
     
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('selldatebreaks', CollectionType::class, array(
-        	    'entry_type' => TourBreakType::class,
-        	    'allow_add' => true,
-        	    'allow_delete' => true,
+            ->add('start', DateType::class)
+            ->add('end', DateType::class)
+            ->add('sellamounts', CollectionType::class, array(
+        	    'entry_type' => TourSellType::class,
         	    'prototype' => true,
         	    'by_reference' => false,
             ))
-            ->add('submit', SubmitType::class,array(
-                "label" => "Update"))
-            
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => \App\Entity\Tour::class,
+            'data_class' => \App\Entity\SellDateBreak::class,
         ));
     }
 
     public function getBlockPrefix()
     {
-        return 'ot_toursells';
+        return 'ot_tourbreak';
     }
 }
--- src/Form/Type/TourSellType.php
+++ src/Form/Type/TourSellType.php
@@ -6,37 +6,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class TourSellsType extends AbstractType
+class TourSellType extends AbstractType
 {
     
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('selldatebreaks', CollectionType::class, array(
-        	    'entry_type' => TourBreakType::class,
-        	    'allow_add' => true,
-        	    'allow_delete' => true,
-        	    'prototype' => true,
-        	    'by_reference' => false,
-            ))
-            ->add('submit', SubmitType::class,array(
-                "label" => "Update"))
-            
+            ->add('amount', TextType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => \App\Entity\Tour::class,
+            'data_class' => \App\Entity\SellAmount::class,
         ));
     }
 
     public function getBlockPrefix()
     {
-        return 'ot_toursells';
+        return 'ot_toursell';
     }
 }
